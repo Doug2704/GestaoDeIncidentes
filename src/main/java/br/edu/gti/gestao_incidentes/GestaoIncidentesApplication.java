72642642cf536ffca927eb09d36a595efcc1e3b3
candido <doug.candido2704@gmail.com>
2025-04-03 13:28:50 -0300

    feat: estrutura do projeto criada.

---
+++ src/main/java/br/edu/gti/gestao_incidentes/GestaoIncidentesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.gti.gestao_incidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoIncidentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoIncidentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/gti/gestao_incidentes/GestaoIncidentesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gti.gestao_incidentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoIncidentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
