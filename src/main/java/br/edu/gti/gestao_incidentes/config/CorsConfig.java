e24f5a3b6b336b0851a59bdd794c9358c3fdadb0
candido <doug.candido2704@gmail.com>
2025-05-29 05:08:18 -0300

    refactor: change corsconfig to try give access to all origins

--- src/main/java/br/edu/gti/gestao_incidentes/config/CorsConfig.java
+++ src/main/java/br/edu/gti/gestao_incidentes/config/CorsConfig.java
@@ -4,21 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")        // Libera para qualquer origem
-                        .allowedMethods("*")        // Libera todos os métodos (GET, POST, etc.)
-                        .allowedHeaders("*")        // Libera todos os headers
-                        .allowCredentials(false);   // Não envia cookies (requerido com "*")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
 }
+
